09045e55c16a497631ec4e1e565ad1046cb11d93
Thinh111409 <99113767+Thinh111409@users.noreply.github.com>
2022-02-06 14:16:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
-    titleWeb: "Gửi Gơn Linh
+    titleWeb: "Gửi Gơn Linh''
     introTitle: 'Khánh Linh'
     introDesc: `Là 1 boiz tỉnh táo
     T không mê tín 
-    T mê Khánh Linh
+    T mê Khánh Linh',
     btnIntro: '^^HiHi^^',
     title: 'Duyên trời sắp đặt rồi',
     desc: 'Thích thì lói 1 tiếng đê',
@@ -12,7 +12,7 @@ const CONFIG = {
     question: 'Boiz Lạnh Lùng yêu Gơn bị khùng',
     btnReply: 'Gửi cho Thịnh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'T biết ngay mà :3'
+    mess: 'T biết ngay mà :3',
     messDesc: 'Có cơ hội thì gặp nhau nhó:v',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
