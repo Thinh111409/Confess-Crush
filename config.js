abd27b3f4ceb21c5bc48ccfc6be8bd36db03c58e
Thinh111409 <99113767+Thinh111409@users.noreply.github.com>
2022-02-06 14:22:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 const CONFIG = {
-    titleWeb: "Gửi Gơn Linh''
-    introTitle: 'Khánh Linh'
-    introDesc: 'Là 1 boiz tỉnh táo
-    'T không mê tín 
-    'T mê Khánh Linh',
+    titleWeb: "Tiêu đề web",
+    introTitle: 'Tên người yêu bạn',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Duyên trời sắp đặt rồi',
-    desc: 'Thích thì lói 1 tiếng đê',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Boiz Lạnh Lùng yêu Gơn bị khùng',
-    btnReply: 'Gửi cho Thịnh <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'T biết ngay mà :3',
-    messDesc: 'Có cơ hội thì gặp nhau nhó:v',
+    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://github.com/zukahai/Confess-Crush' 
 }
